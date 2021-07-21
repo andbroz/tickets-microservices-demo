6000401be316d22d35acd14476c2fad2dca06b32
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2021-07-21 22:12:52 +0200

    Initial commit

---
+++ auth/src/index.ts
@@ -0,0 +1,27 @@
+require('dotenv').config();
+import express from 'express';
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/sign-in';
+import { signOutRouter } from './routes/sign-out';
+import { signUpRouter } from './routes/sign-up';
+
+const PORT = process.env.PORT ?? 3000;
+const app = express();
+
+app.use(express.json());
+
+/**
+ * ROUTES
+ */
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+/**
+ * LISTEN
+ */
+app.listen(PORT, () => {
+  console.log(`Auth service listening on http://localhost:${PORT}`);
+});
