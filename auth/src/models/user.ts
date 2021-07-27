fb5f4ef27a4736e212754d85ea86b1fda31c78be
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2021-07-27 07:34:08 +0200

    add static typing to model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,17 @@
 import mongoose from 'mongoose';
 
+// Interface that describe properties required
+// to create a new User
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// Interface describing properties of usermodel
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): any;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -11,6 +23,10 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<any, UserModel>('User', userSchema);
 
 export { User };
