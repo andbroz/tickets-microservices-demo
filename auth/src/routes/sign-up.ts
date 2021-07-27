069e55a9beda775b0c831b8b7f29e3f9dd22cc5c
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2021-07-27 07:54:41 +0200

    add saving user to db at signup

--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { User } from '../models/user';
 
 const router = express.Router();
 
@@ -15,19 +15,26 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password length must be between 4 to 20 characters'),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
-
     const { email, password } = req.body;
 
-    console.log(`Creating user ${email} - ${password}`);
-    throw new DatabaseConnectionError();
+    // check if user exist
+    const existingUser = await User.findOne({ email });
 
-    res.send({});
+    if (existingUser) {
+      console.log('Email in use:', email);
+      return res.send({});
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   },
 );
 
