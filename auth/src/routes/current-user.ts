062f9259d01aa9a1cad97fa4ec50ddd1a5ad0ab3
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2021-12-29 14:31:09 +0100

    add current user and singout response

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,21 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { StatusCodes } from 'http-status-codes';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('helo!!!');
+  // check if user have a jwt set
+  if (!req.session?.jwt) {
+    return res.status(StatusCodes.UNAUTHORIZED).send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (error) {
+    res.status(StatusCodes.UNAUTHORIZED).send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
--- auth/src/routes/sign-out.ts
+++ auth/src/routes/sign-out.ts
@@ -3,7 +3,8 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-  res.send('goodbye');
+  req.session = null;
+  res.send({});
 });
 
 export { router as signOutRouter };
