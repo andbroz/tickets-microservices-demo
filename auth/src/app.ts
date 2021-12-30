6369811202f38e6750d74e6768093b7d76c3d5b6
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2021-12-30 20:18:18 +0100

    setup tests

---
+++ auth/src/app.ts
@@ -0,0 +1,47 @@
+require('dotenv').config();
+import cookieSession from 'cookie-session';
+import express from 'express';
+import 'express-async-errors';
+import { NotFoundError } from './errors/not-found-error';
+import { errorHandler } from './middlewares/error-handler';
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/sign-in';
+import { signOutRouter } from './routes/sign-out';
+import { signUpRouter } from './routes/sign-up';
+
+const app = express();
+
+// https://expressjs.com/en/5x/api.html#trust.proxy.options.table
+// express is behind ingress-nginx and need to trust that proxy
+app.set('trust proxy', true);
+app.set('x-powered-by', false);
+
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  }),
+);
+
+/**
+ * ROUTES
+ */
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+// Not found route error handler
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+/**
+ * Middleware
+ */
+
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,53 +1,9 @@
-require('dotenv').config();
-import express from 'express';
-import 'express-async-errors';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signInRouter } from './routes/sign-in';
-import { signOutRouter } from './routes/sign-out';
-import { signUpRouter } from './routes/sign-up';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const PORT = process.env.PORT ?? 3000;
-const app = express();
-
-// https://expressjs.com/en/5x/api.html#trust.proxy.options.table
-// express is behind ingress-nginx and need to trust that proxy
-app.set('trust proxy', true);
-app.set('x-powered-by', false);
-
-app.use(express.json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  }),
-);
-
-/**
- * ROUTES
- */
-
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signUpRouter);
-
-// Not found route error handler
-app.all('*', async (req, res) => {
-  throw new NotFoundError();
-});
-
-/**
- * Middleware
- */
-
-app.use(errorHandler);
+import { app } from './app';
 
 /** Database connection */
+
+const PORT = process.env.PORT ?? 3000;
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
