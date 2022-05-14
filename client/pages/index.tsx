3c0b31cb7b70c91655454015f2024362abbde18a
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2022-05-15 01:14:29 +0200

    add home page with auth

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,39 @@
-import type { NextPage } from 'next';
+import type { InferGetServerSidePropsType, NextPage, NextPageContext } from 'next';
+import { GetServerSideProps } from 'next';
+import axios from 'axios';
 
-const Home: NextPage = () => {
-  return <h1 className='bg-emerald-800 text-2xl text-white'>Home</h1>;
+export const baseURL = 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local';
+
+type User = {
+  currentUser: {
+    id: string;
+    email: string;
+    iat: number;
+  } | null;
 };
+
+export const getServerSideProps: GetServerSideProps<User> = async ({ req }) => {
+  try {
+    const response = await axios.get<User>(`${baseURL}/api/users/currentuser`, {
+      headers: {
+        host: req.headers.host ?? 'ticketing.dev.local',
+        cookie: req.headers.cookie ?? '',
+      },
+    });
+    return { props: response.data };
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err.message);
+    }
+  }
+
+  return { props: { currentUser: null } };
+};
+
+function Home(data: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(data.currentUser);
+
+  return <h1 className='bg-emerald-800 text-2xl text-white'>Home</h1>;
+}
 
 export default Home;
